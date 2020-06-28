55089b377bb883a863eafa24dd837fb5a4c817a8
Pavan N <pavann1401@gmail.com>
2020-06-28 12:44:55 +0530

    now shows user_id for SingUp

--- movie_reccomendation_genre.py
+++ movie_reccomendation_genre.py
@@ -20,18 +20,9 @@ df = pd.read_csv("movies.csv")
 def searchbygenre(search):
     movielist = []
     dftolist = df.values.tolist()
-    #search=input ("Enter genre: ")
     for i in range (len(dftolist)):
             if search.lower() in dftolist[i][2].lower():
                 movielist.append(dftolist[i][1])
-        #print (">>>Here are some recomendations  for you:")
-        #for movie in random.sample(movielist,5): #% random suggestions
-        #    print (movie)
-        #ch = input("Are you satisfied with the suggestions ?")
-        #if 'y' in ch.lower():
-        #    q = True
-        #else:
-        #    print (">>Hope you like these suggestions ")
+        
     return movielist
 
-# %%
